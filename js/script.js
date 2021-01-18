0ba3e74af26b74fbd2d3f82a4ec89246f05838a4
nat-5 <71806085+nat-5@users.noreply.github.com>
2021-01-18 11:18:56 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// VARIABLES Y TIPOS DE DATOS EN JAVASCRIPT
+var  saludo  =  "Hola JavaScript"
+var  edad  =  26
+var  unPoco  =  1,5
+var  siNo  =  verdadero
+var  seccion  =  'a'
+
+// MOSTRAR UN DATO EN CONSOLO
+consola . log ( saludo )
+
+
+var  datoAñadir  =  documento . getElementById ( 'favorito' )
